96611e8739626b2644be3c356596ccba497d3d5f
SubuMangu <142916612+SubuMangu@users.noreply.github.com>
2024-05-24 12:54:13 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,75 @@
+import pickle
+import streamlit as st
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+CLIENT_ID = "363113542be846e58f4f4090b3799b7b"
+CLIENT_SECRET = "2dc65faaa3fd4650ba0c8c5dc5e16c49"
+
+# Initialize the Spotify client
+client_credentials_manager = SpotifyClientCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+def get_song_album_cover_url(song_name, artist_name):
+    search_query = f"track:{song_name} artist:{artist_name}"
+    results = sp.search(q=search_query, type="track")
+
+    if results and results["tracks"]["items"]:
+        track = results["tracks"]["items"][0]
+        album_cover_url = track["album"]["images"][0]["url"]
+        print(album_cover_url)
+        return album_cover_url
+    else:
+        return "https://i.postimg.cc/0QNxYz4V/social.png"
+
+def recommend(song):
+    index = music[music['song'] == song].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_music_names = []
+    recommended_music_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        artist = music.iloc[i[0]].artist
+        print(artist)
+        print(music.iloc[i[0]].song)
+        recommended_music_posters.append(get_song_album_cover_url(music.iloc[i[0]].song, artist))
+        recommended_music_names.append(music.iloc[i[0]].song)
+
+    return recommended_music_names,recommended_music_posters
+st.image('Spotify_Logo_RGB_Green.png') 
+st.header('Music Recommender System')
+music = pickle.load(open('df.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+music_list = music['song'].values
+selected_movie = st.selectbox(
+    "Type or select a song from the dropdown",
+    music_list
+)
+st.sidebar.header('**Music Recommendation System**')
+st.sidebar.markdown('''Made with ❤️ by Subrat Mohapatra 
+                    \n a 2nd yr undergrad at OUTR,Bbsr
+                    ''')
+st.sidebar.header('Contacts📞')
+st.sidebar.markdown('''- [LinkedIn](https://www.linkedin.com/in/subrat-mohapatra2003)''')
+st.sidebar.markdown('''- [Twitter](https://x.com/SubratM68912500)''')
+st.sidebar.markdown(''' - Email: subratmohapatra2003@gmail.com''')
+if st.button('Show Recommendation'):
+    recommended_music_names,recommended_music_posters = recommend(selected_movie)
+    col1, col2, col3, col4, col5= st.columns(5)
+    with col1:
+        st.text(recommended_music_names[0])
+        st.image(recommended_music_posters[0])
+    with col2:
+        st.text(recommended_music_names[1])
+        st.image(recommended_music_posters[1])
+
+    with col3:
+        st.text(recommended_music_names[2])
+        st.image(recommended_music_posters[2])
+    with col4:
+        st.text(recommended_music_names[3])
+        st.image(recommended_music_posters[3])
+    with col5:
+        st.text(recommended_music_names[4])
+        st.image(recommended_music_posters[4])
